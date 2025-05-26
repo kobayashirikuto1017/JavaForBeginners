b112b9f6946c692a5efcf16bd0fbedfa2aa5857b
DESKTOP-BJ2LRPE\User <riku.koba1017@gmail.com>
2025-05-26 11:29:55 +0900

    車のおもちゃ

---
+++ src/RectandOval.java
@@ -0,0 +1,20 @@
+
+public class RectandOval extends MyFrame{
+	public  void  run(){
+		setColor(0,250,0);
+		  fillRect(130,40,100,50);
+		setColor(0,250,0);
+		  fillRect(100,80,160,50);
+		setColor(0,0,0);
+		fillOval(120,130,30,30);
+		setColor(0,0,0);
+		fillOval(210,130,30,30);
+		
+		
+		setColor(0,200,0);
+		  fillRect(200,180,120,60);
+		  setColor(0,200,0);
+		  fillRect(205,220,110,100);
+		 
+	}
+}
--- src/Start.java
+++ src/Start.java
@@ -6,7 +6,7 @@ public class Start {
     System.out.println("プログラムを開始します");
     
    
-    MyFrame frame１ = new TextTest();
+    MyFrame frame１ = new RectandOval();
 	}
   
 }
