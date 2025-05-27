390f53504bf68b9f0b3a878885b9434e52d45bcf
DESKTOP-BJ2LRPE\User <riku.koba1017@gmail.com>
2025-05-27 11:01:38 +0900

    5-2

--- src/DisplayRectsSlow.java
+++ src/DisplayRectsSlow.java
@@ -3,7 +3,9 @@ public class DisplayRectsSlow extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
 		int x = 30;
+//		5-1
 		while(x<=170) {
+			
 			fillRect(x,80+x,10,100);
 			x+=20;
 			sleep(1);
--- src/Start.java
+++ src/Start.java
@@ -6,7 +6,7 @@ public class Start {
     System.out.println("プログラムを開始します");
     
    
-    MyFrame frame１ = new DisplayRectsSlow();
+    MyFrame frame１ = new Animation();
 	}
   
 }
